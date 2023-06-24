f923898edf67d067c9f6e3eecf512b315f8bbe7b
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-24 12:46:30 +0200

    change statuses

--- index.ts
+++ index.ts
@@ -9,8 +9,8 @@ export default {
 
         if (request.method === "POST") {
             const { body, name }: POSTPayload = await request.json();
-            if (!body) return new Response("Missing body.", { status: 500, headers });
-            if (body.length > 50) return new Response("Body is too long.", { status: 500, headers });
+            if (!body) return new Response("Missing body.", { status: 400, headers });
+            if (body.length > 50) return new Response("Body is too long.", { status: 400, headers });
 
             try {
                 await env.DB.prepare("INSERT INTO entries (id, date, body, name) VALUES (?1, ?2, ?3, ?4)").bind(crypto.randomUUID(), new Date().getTime(), body, name ?? null).run();
@@ -32,7 +32,7 @@ export default {
         if (request.method === "DELETE") {
             // https://github.com/cloudflare/workers-sdk/issues/1388
             const idToDelete = request.headers.get("id-to-delete");
-            if (!idToDelete) return new Response("Missing id.", { status: 500, headers });
+            if (!idToDelete) return new Response("Missing id.", { status: 400, headers });
 
             try {
                 await env.DB.prepare("DELETE FROM entries WHERE id = ?1").bind(idToDelete).run();
